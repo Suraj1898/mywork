503374fead95b644fe2cab5171e56a0625e445c8
Suraj1898 <145121608+Suraj1898@users.noreply.github.com>
2023-09-15 17:07:03 +0530

    Add files via upload

---
+++ data sceince class.R
@@ -0,0 +1,51 @@
+print(10)
+print(10*5)
+a <- 10*5
+a
+coursename<-"da"
+course_name <-"da"
+Course_name <- "ds"
+a_ <-"dk"
+rm(course_name)
+rm(course_name)
+rm(coursename)
+rm(Course_name)
+rm(a)
+ten <- 10L
+class(ten)
+FLOA <- 10.5
+class(FLOA)
+CITY <- "PUNE"
+class(CITY)
+
+PINCODE<-"460559"
+class(PINCODE)
+
+
+BOOLEAN <- "TRUE"
+class(BOOLEAN)
+NAME <- "SURAJ SINGH BHADOURIA"
+AGE <- 10
+
+#DATA STRUCTURE
+
+#HOMOGENOUS DATA STRUCTURE
+
+AGE <- c(20,21,22,23,24,25,26)
+
+info <- c('sakshi',25,145,32,TRUE)
+#accessing element from vector
+
+info[2]
+AGE[4]
+AGE[1:6]
+info[2:4]
+info[c(1,3)]
+info[c(1,5)]
+AGE[c(1,3,7)]
+
+#assigning or replacing values in a vector
+
+info[2] <-"22"
+info[c(1,5)] <- c("rani","false")
+info[c(1,5)]
